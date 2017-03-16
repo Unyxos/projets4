e08ace5077db7420690f4af4695cc2e412782e79
Unyxos <corentin.closs@gmail.com>
2017-03-16 09:17:53 +0100

    fix ajout d'utilisateur depuis le backoffice

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,7 +7,14 @@ use Symfony\Component\Debug\Debug;
 // read http://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
 //umask(0000);
-
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+    '192.168.33.1',
+    '::1',
+    '149.91.91.176',
+))) {
+    header('HTTP/1.0 403 Forbidden');
+    exit('This script is only accessible from localhost.');
+}
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
