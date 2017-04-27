e81d4045fed44473d626fc38641e19fb23b1f2e3
Corentin Closs <me@corentincloss.fr>
2017-04-27 10:52:39 +0200

    Delete clear.sh

--- clear.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-rm composer.lock
-git pull origin master
-composer update
-php bin/console doctrine:schema:update --force --dump-sql
-php bin/console cache:clear --env=prod
-chmod -R 777 var
